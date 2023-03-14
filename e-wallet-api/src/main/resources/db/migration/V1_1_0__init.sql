3c7929e9f3d03021725ff9a94aff5e0431879406
a.pavliychenkov <a.pavliychenkov@gmail.com>
2023-03-14 13:10:32 +0100

    removed comments

--- e-wallet-api/src/main/resources/db/migration/V1_1_0__init.sql
+++ e-wallet-api/src/main/resources/db/migration/V1_1_0__init.sql
@@ -20,8 +20,5 @@ create table if not exists users
 
 create extension if not exists "uuid-ossp";
 
--- insert into wallets(id, balance, currency_type, name, user_id)
--- values ('uuid_generate_v4()', 0.00, 'USD', 'Main wallet', '859b7b18-905a-4ff9-b5ac-51a34f4ebb9e');
-
 insert into users(id, login, password, role)
 values (uuid_generate_v4(), 'admin', '$2a$10$hdxa9S.O7TVShSZbQVcF4.JY5Pdd0vhJ9a6TdaaYQ.7LnuDKXPvWm', 'ADMIN')
